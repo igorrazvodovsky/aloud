6e337163d88358200075fc76dd4f9f64122dc33a
Igors Razvodovskis <igors.razvodovskis@Kuehne-Nagel.com>
2020-05-05 17:24:01 +0300

    Remove demo of search layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,24 @@
-<!-- 1. Checks the book & sets a default -->
-<!-- 2. Mobile/desktop components switch -->
-<!-- 3. Main view & bottom sheet switch -->
-
 // TODO: A better way to set a default?
 
 <template>
-  <v-app class="player" v-if="book.metadata.title !== ''">
-    <!-- Player or search -->
-    <v-slide-x-reverse-transition leave-absolute hide-on-leave>
-      <player-desktop-view
-        key="1"
-        v-if="player && !isMobile"
-        @open-player="toggleBrowser"
-        :closed="browser"
-      />
-      <player-mobile-view
-        key="1"
-        v-if="player && isMobile"
-        @open-player="toggleBrowser"
-        :closed="browser"
-      />
-      <search key="2" v-if="page == 'search'" />
-    </v-slide-x-reverse-transition>
-
-    <!-- Browser or search results -->
-    <v-slide-y-reverse-transition>
-      <browser key="1" v-if="browse" @open-browser="toggleBrowser" :open="browser" />
-      <search-results key="2" v-if="page == 'search'" />
-    </v-slide-y-reverse-transition>
+  <v-app class="player" v-if="bookDataLoaded">
+    <player-desktop-view
+      v-if="player && !isMobile"
+      @open-player="toggleBrowser"
+      :closed="browser"
+    />
+    <player-mobile-view
+      v-if="player && isMobile"
+      @open-player="toggleBrowser"
+      :closed="browser"
+    />
+    <browser v-if="browse" @open-browser="toggleBrowser" :open="browser" />
   </v-app>
 </template>
 
 <script>
 import PlayerMobileView from "~/components/player/player-mobile-view.vue";
 import PlayerDesktopView from "~/components/player/player-desktop-view.vue";
-import Search from "~/components/search/search.vue";
-import SearchResults from "~/components/search/search-results.vue";
 import Browser from "~/components/browse/browser.vue";
 import { mapState, mapMutations } from "vuex";
 import axios from "axios";
@@ -44,9 +27,7 @@ export default {
   components: {
     Browser,
     PlayerMobileView,
-    PlayerDesktopView,
-    Search,
-    SearchResults
+    PlayerDesktopView
   },
 
   data() {
@@ -63,6 +44,9 @@ export default {
     // TODO: Better name
     browse() {
       return this.page == "index" || this.page == "browse";
+    },
+    bookDataLoaded() {
+      return !this.book.metadata.title == "";
     }
   },
   mounted() {
